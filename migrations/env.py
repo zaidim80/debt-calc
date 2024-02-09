ffefd9892ffc8d4f9c35ca715fbb52ae19bf66b8
zaidim <zaidim@mail.ru>
2024-02-09 22:28:16 +0500

    docker-compose, alembic cfg, models + fastapi

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.config import cfg
+from src.models import metadata
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -14,11 +17,13 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option('sqlalchemy.url', cfg.dsn)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ src/models.py
@@ -0,0 +1,35 @@
+import sqlalchemy as sa
+
+
+metadata = sa.MetaData()
+
+
+user= sa.Table(
+    'user',
+    metadata,
+    sa.Column('email', sa.String(200), primary_key=True),
+    sa.Column('name', sa.String(200), nullable=False),
+    sa.Column('admin', sa.Boolean, nullable=False, server_default=sa.false()),
+)
+
+
+debt= sa.Table(
+    'debt',
+    metadata,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('name', sa.String(100), nullable=False),
+    sa.Column('date', sa.Date, nullable=False),
+    sa.Column('amount', sa.Integer, nullable=False),
+    sa.Column('period', sa.Intger, nullable=False),
+)
+
+
+payment= sa.Table(
+    'payment',
+    metadata,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('debt_id', sa.Integer, sa.ForeignKey('debt.id'), nullable=False),
+    sa.Column('date', sa.DateTime, nullable=False),
+    sa.Column('amount', sa.Integer, nullable=False),
+    sa.Column('author_email', sa.String(200), sa.ForeignKey('user.email'), nullable=False),
+)
