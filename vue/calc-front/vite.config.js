4faf16504fc72e06e1d77d467a3c4305a88a9b9b
Dmitry Zaitsev <zaidim80@gmail.com>
2024-05-04 17:06:26 +0500

    подчистил front

--- vue/calc-front/vite.config.js
+++ vue/calc-front/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    server: {
+        port: 8001,
+        host: true,
+        watch: {usePolling: true},
+    },
+    plugins: [vue()],
+});
