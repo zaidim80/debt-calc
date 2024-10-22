08432e1dfa728ead163b8222daf4df3e2cb2e18f
Zaitsev Dmitry <dazaitsev@urfu.ru>
2024-10-22 13:50:01 +0500

    задел под авторизацию

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,23 +1,32 @@
-import { createApp } from 'vue';
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-import App from './App.vue';
-import './style.css';
+import App from "./App.vue";
+import "./style.css";
 
-import HomePage from './components/home-page.vue';
-import LoginPage from './components/login-page.vue';
+import HomePage from "./components/home-page.vue";
+import LoginPage from "./components/login-page.vue";
 
 const routes = [
-    { path: '/', component: HomePage, },
-    { path: '/login', component: LoginPage, },
+    { path: "/", component: HomePage, meta: { requiresAuth: true }, },
+    { path: "/login", component: LoginPage, },
 ];
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem("token");
+    if (to.matched.some((route) => route.meta.requiresAuth)) {
+        if (token) { next(); } else { next("/login"); }
+    } else {
+        next();
+    }
 });
 
 createApp(App)
